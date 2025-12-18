1a94c6817568d72f81971e28cb8b8dba39ffd4e7
anwaar-kokeh <anwaarkokeh@gmail.com>
2025-12-18 18:43:37 +0200

    add (id, fromAccount, toAccount, type) fields and getters to Transaction class, and add transaction type enum

--- src/transactions/Transaction.java
+++ src/transactions/Transaction.java
@@ -1,9 +1,33 @@
 package transactions;
 
+import accounts.Account;
+
 public class Transaction {
     public double amount;
-
+    private String id;
+    private Account fromAccount;
+    private Account toAccount;
+    private TransactionType type;
     public Transaction(double amount) {
         this.amount = amount;
+    }
+
+    public Account getToAccount() {
+        return this.toAccount;
+    }
+    public Account getFromAccount() {
+        return this.fromAccount;
+    }
+
+    public TransactionType getType() {
+        return this.type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
     }
 }
